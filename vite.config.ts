843400b4ac5a0d82aa140fff0adae0dc342e8cb2
liam-wilbur <liamcwilbur@gmail.com>
2025-08-05 23:02:44 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/Mango-Moon/',
   server: {
     host: "::",
     port: 8080,
